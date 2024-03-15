abaddc87a33cb2bc5cd2328c3b9e6f5462b1f1f8
skvicky2 <30534790+skvicky2@users.noreply.github.com>
2024-03-15 17:44:42 +0530

    Update settings.py

--- understand_science/understand_science/settings.py
+++ understand_science/understand_science/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r@*&o^cy1nd@&y47ppi7fonorxbfgdmm&&818eb%%m2xqns@7!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "18.205.155.104",
