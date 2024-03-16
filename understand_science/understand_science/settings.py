3925caa40b49d02579aff0434be4cc0906d7f685
skvicky2 <skvicky2@gmail.com>
2024-03-16 16:14:40 +0530

    Media file final testing commit

--- understand_science/understand_science/settings.py
+++ understand_science/understand_science/settings.py
@@ -201,4 +201,3 @@ AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
