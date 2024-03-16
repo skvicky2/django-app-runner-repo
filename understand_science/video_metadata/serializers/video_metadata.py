44aa6ec71fd628273108e47fc021ec867c91d9dd
skvicky2 <skvicky2@gmail.com>
2024-03-16 13:27:24 +0530

    Menu data in category object

--- understand_science/video_metadata/serializers/video_metadata.py
+++ understand_science/video_metadata/serializers/video_metadata.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from video_metadata.models import VideoMetadata
 from uts.models import Category, Author, Tag
+from site_admin_settings.models import Menu
+
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    
+    menu = serializers.SerializerMethodField()
+    
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ["title", "description", "menu", "created_by", "created_date", "updated_date"]
         
+    def get_menu(self, obj):
+        main_menu = Menu.objects.filter(sub_menu=obj.id).first()
+        if main_menu:
+            return main_menu.menu
+        return ""
+    
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
